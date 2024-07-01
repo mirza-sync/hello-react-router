bef8130d6a29264a6a2c23cc2eeed29b2b4688ca
mirza-sync <mirzasyahmi123@gmail.com>
2024-07-01 22:37:11 +0800

    Add root route & error handling

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Root from './routes/root'
+import ErrorPage from './error-page'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
