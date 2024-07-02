3c0372bb908acd42dc9c8a48112b06b7d2b953f4
mirza-sync <mirzasyahmi123@gmail.com>
2024-07-03 00:00:06 +0800

    Add delete contact action (destroy 😠)

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import Root, { loader as rootLoader, action as rootAction } from './routes/root'
 import ErrorPage from './error-page'
 import Contact, { loader as contactLoader } from './routes/contact'
 import EditContact, { action as editAction } from './routes/edit'
+import { action as destroyAction } from './routes/destroy'
 
 const router = createBrowserRouter([
   {
@@ -26,6 +27,11 @@ const router = createBrowserRouter([
         loader: contactLoader,
         action: editAction,
       },
+      {
+        path: '/contacts/:contactId/destroy',
+        action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>,
+      }
     ]
   },
 ])
