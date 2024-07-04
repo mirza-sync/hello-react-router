7d3c226f0f21fedbbcd5ea27d5ecd143aa060862
mirza-sync <mirzasyahmi123@gmail.com>
2024-07-04 09:52:25 +0800

    Feat: Search contacts

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,9 +1,12 @@
-import { Form, Link, NavLink, Outlet, redirect, useLoaderData, useNavigation } from "react-router-dom";
+import { Form, Link, NavLink, Outlet, redirect, useLoaderData, useNavigation, useSubmit } from "react-router-dom";
 import { createContact, getContacts } from "../contacts";
+import { useEffect } from "react";
 
-export async function loader() {
-    const contacts = await getContacts()
-    return { contacts }
+export async function loader({ request }) {
+    const url = new URL(request.url)
+    const q = url.searchParams.get('q')
+    const contacts = await getContacts(q)
+    return { contacts, q }
 }
 
 export async function action() {
@@ -12,21 +15,28 @@ export async function action() {
 }
 
 export default function Root() {
-    const { contacts } = useLoaderData()
+    const { contacts, q } = useLoaderData()
     const navigation = useNavigation()
+    const submit = useSubmit()
+
+    useEffect(() => {
+        document.getElementById('q').value = q
+    }, [q])
 
     return (
         <>
             <div id="sidebar">
                 <h1>React Router Contacts</h1>
                 <div>
-                    <form id="search-form" role="search">
+                    <Form id="search-form" role="search">
                         <input
                             id="q"
                             aria-label="Search contacts"
                             placeholder="Search"
                             type="search"
                             name="q"
+                            defaultValue={q}
+                            onChange={(e) => submit(e.currentTarget.form)}
                         />
                         <div
                             id="search-spinner"
@@ -37,7 +47,7 @@ export default function Root() {
                             className="sr-only"
                             aria-live="polite"
                         ></div>
-                    </form>
+                    </Form>
                     <Form method="post">
                         <button type="submit">New</button>
                     </Form>
