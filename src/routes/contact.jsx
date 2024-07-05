07ed082c3250ceed221f99afecddad84eb8adf51
mirza-sync <mirzasyahmi123@gmail.com>
2024-07-05 10:45:36 +0800

    Feat: Set contact to favorite (mutation w/o navigation)

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,9 +1,16 @@
-import { Form, useLoaderData } from "react-router-dom";
-import { getContact } from "../contacts";
+import { Form, useFetcher, useLoaderData } from "react-router-dom";
+import { getContact, updateContact } from "../contacts";
 
 export async function loader({ params }) {
-    const contact = await getContact(params.contactId)
-    return { contact }
+  const contact = await getContact(params.contactId)
+  return { contact }
+}
+
+export async function action({ request, params }) {
+  const formData = await request.formData()
+  return updateContact(params.contactId, {
+    favorite: formData.get('favorite') === 'true'
+  })
 }
 
 export default function Contact() {
@@ -72,9 +79,11 @@ export default function Contact() {
 }
 
 function Favorite({ contact }) {
+  const fetcher = useFetcher()
   const favorite = contact.favorite;
+
   return (
-    <Form method="post">
+    <fetcher.Form method="post">
       <button
         name="favorite"
         value={favorite ? "false" : "true"}
@@ -86,6 +95,6 @@ function Favorite({ contact }) {
       >
         {favorite ? "★" : "☆"}
       </button>
-    </Form>
+    </fetcher.Form>
   );
 }
